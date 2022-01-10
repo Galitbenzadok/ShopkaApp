56fabd60fe1d66552886d0f8faa6446b54a26eb6
Galit <87934597+Galitbenzadok@users.noreply.github.com>
2022-01-10 16:29:47 +0200

    Update Menu.js

--- src/components/LayoutArea/Menu/Menu.js
+++ src/components/LayoutArea/Menu/Menu.js
@@ -1,12 +1,31 @@
 import "./Menu.css";
+import img from "../../../assets/depart.PNG";
+import indentIcon from "../../../assets/Vector.png"
 
 function Menu() {
     return (
       <div className="Menu">
-        {/* <a href="#">Home</a>
-        <a href="#">Products</a>
-        <a href="#">About</a> */}
-        <img src="./asaid.png" alt="" className="asaidImg"></img>
+        <img src={img} alt="" />
+        <h2> All Categories </h2>
+
+        <h3>
+          <img src={indentIcon} alt="" className="indentIcon" />
+          Cell Phones and Smartphones
+        </h3>
+        <a href="#">Cell Phone Accessories</a>
+        <a href="#">Cell Phone Gatgets</a>
+        <a href="#">Applications</a>
+        <a href="#">Smart Watches</a>
+        <a href="#">Show more</a>
+        <h3>
+          <img src={indentIcon} alt="" className="indentIcon" />
+          Business and Industrial
+        </h3>
+        <a href="#">Cell Phone Accessories</a>
+        <a href="#">Cell Phone Gatgets</a>
+        <a href="#">Applications</a>
+        <a href="#">Smart Watches</a>
+        <a href="#">Show more</a>
       </div>
     );
 }
